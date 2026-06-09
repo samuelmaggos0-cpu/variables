e9a906479da05f7a9d3766d4e21664340d79d813
samuel maggos <60449560-fixdib@users.noreply.replit.com>
2026-06-09 11:52:08 +0000

    creating variables

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age;
+	age = 10000;
+	int money;
+	money = 340;
+	int score;
+	score = 12;
+	printf("%d and %d and %d\n", age, money, score);
+	return (0);
+}
